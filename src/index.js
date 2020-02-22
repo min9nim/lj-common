190c29bbc9eee10d1ca07bf2e879697b94dca972
mgsong <mgsong@jjsoft.kr>
2020-02-22 15:43:45 +0900

    불필요 세팅 제거..

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
 import VCode from './views/Code.vue'
 
-const install = Vue => {
-  Vue.component(VCode.name, VCode)
-}
-
-// auto install if used in browser
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
-}
-
-export {VCode}
-
-export default install
+export default VCode
