ed23a597d2fdc0d39739a495bba7577b0cb088f3
mgsong <mgsong@jjsoft.kr>
2020-02-22 22:11:46 +0900

    export 오류 수정

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import VCode from './views/Code.vue'
-// import {
-//   setApiServer,
-//   req,
-//   removeBy,
-//   _idAscending,
-//   nameAscending,
-//   idEqual,
-//   findById,
-//   updateBy,
-// } from './utils'
-export * from './utils'
+import {
+  setApiServer,
+  req,
+  removeBy,
+  _idAscending,
+  nameAscending,
+  idEqual,
+  findById,
+  updateBy,
+} from './utils'
+// export * from './utils'  // 이렇게는 처리가 안 됨
 
-export {VCode}
+export {VCode, setApiServer, req, removeBy, nameAscending, idEqual, findById, updateBy}
