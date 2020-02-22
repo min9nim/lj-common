f4db285c4928405ef1c7e42f0e883fa66380b759
mgsong <mgsong@jjsoft.kr>
2020-02-23 01:00:06 +0900

    remove source-map when `yarn build:lib`

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
+const {default: createLogger} = require('if-logger')
+const logger = createLogger().addTags('vue.config.js')
+
 module.exports = {
   configureWebpack: config => {
+    logger.addTags('webpack').info('devtool: ' + config.devtool)
+    if (process.env.npm_lifecycle_event === 'build:lib') {
+      config.devtool = false
+      logger.addTags('webpack').info('devtool(tobe): ' + config.devtool)
+    }
     config.module.rules.push({
       test: /\.pug$/,
       loader: 'pug-plain-loader',
