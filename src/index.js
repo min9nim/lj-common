b8ca505d15a9aec5b0133f98ec0f3c7f286a68be
mgsong <mgsong@jjsoft.kr>
2020-02-22 22:07:14 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import VCode from './views/Code.vue'
-import {setApiServer, req} from './utils'
+// import {
+//   setApiServer,
+//   req,
+//   removeBy,
+//   _idAscending,
+//   nameAscending,
+//   idEqual,
+//   findById,
+//   updateBy,
+// } from './utils'
+export * from './utils'
 
-export {VCode, setApiServer, req}
+export {VCode}
