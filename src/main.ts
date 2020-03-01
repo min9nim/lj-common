7e54213951a27f90404360292cf9d7d29dae4668
mgsong <mgsong@jjsoft.kr>
2020-03-02 07:58:29 +0900

    diet

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import VueCompositionApi from '@vue/composition-api'
 // import ElementUI from 'element-ui'
 import Input from 'element-ui/lib/input'
 import Tag from 'element-ui/lib/tag'
+import Tabs from 'element-ui/lib/tabs'
+import TabPane from 'element-ui/lib/tab-pane'
 import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
 // import locale from 'element-ui/lib/locale/lang/ko'
@@ -16,6 +18,8 @@ Vue.config.productionTip = false
 Vue.use(VueCompositionApi)
 Vue.use(Input)
 Vue.use(Tag)
+Vue.use(Tabs)
+Vue.use(TabPane)
 
 new Vue({
   router,
