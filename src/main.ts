a8615008a68d2b42fa6e833116307b0b8ee5d2c4
mgsong <mgsong@jjsoft.kr>
2020-03-02 07:56:18 +0900

    diet

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueCompositionApi from '@vue/composition-api'
-import ElementUI from 'element-ui'
+// import ElementUI from 'element-ui'
+import Input from 'element-ui/lib/input'
+import Tag from 'element-ui/lib/tag'
 import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
-import locale from 'element-ui/lib/locale/lang/ko'
+// import locale from 'element-ui/lib/locale/lang/ko'
 import {setApiServer} from '@/biz'
 
 setApiServer()
 
 Vue.config.productionTip = false
 Vue.use(VueCompositionApi)
-Vue.use(ElementUI, {locale})
+Vue.use(Input)
+Vue.use(Tag)
 
 new Vue({
   router,
