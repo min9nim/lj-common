4037fd1af0cf19b942f220e69534b4860a0d9f30
mgsong <mgsong@jjsoft.kr>
2020-03-03 07:17:04 +0900

    소스맵 다시 생성

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,10 @@ const logger = createLogger().addTags('vue.config.js')
 module.exports = {
   configureWebpack: config => {
     logger.addTags('webpack').info('devtool: ' + config.devtool)
-    if (process.env.npm_lifecycle_event === 'build:lib') {
-      config.devtool = false
-      logger.addTags('webpack').info('devtool(tobe): ' + config.devtool)
-    }
+    // if (process.env.npm_lifecycle_event === 'build:lib') {
+    //   config.devtool = false
+    //   logger.addTags('webpack').info('devtool(tobe): ' + config.devtool)
+    // }
     config.module.rules.push({
       test: /\.pug$/,
       loader: 'pug-plain-loader',
